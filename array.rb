a4babf7f3ca3fbc8f1f4605613889c613d6eb7f8
Sana Joshi <sanajoshi@Johns-MacBook-1.local>
2017-08-04 13:43:35 +1000

    vim is terrible

---
+++ array.rb
@@ -0,0 +1,21 @@
+shoppingList = [
+"chips",
+"coke",
+"water",
+"cheddar",
+"water",
+
+alcohal=[
+"Tooheys old",
+"Rum",
+"Gin",
+"Tequila",
+"whiskey"
+
+]
+
+]
+chosenCheese=shoppingList[3]
+ListCount=shoppingList.length 
+puts "My choosen cheese is #{chosenCheese}"
+puts "your shopping List has #{ListCount}items in it"
